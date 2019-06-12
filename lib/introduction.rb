4fa120048f17bc107e91d04749b8e8353978de32
captivating-form-0787 <avertrees447@gmail.com>
2019-06-12 15:54:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,9 @@ def intruduction(name)
 end
 
 def intruduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 def intruduction_with_language_optional(name, language="Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
